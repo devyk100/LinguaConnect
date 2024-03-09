b68c53cbd65f55c777cf7d4c13b2fa8bd995fe75
Yash Ramesh Kumar <devyk100@gmail.com>
2024-03-09 14:49:51 +0530

    designing the frontend more

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -5,6 +5,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FlashCard from "./components/FlashCard";
 import FlashCardContainer from "./components/FlashCardContainer";
+import Landing from "./pages/Landing";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,6 +15,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/flashcard" element={<FlashCardContainer />}/>
+          <Route path="/" element={<Landing />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ apps/frontend/src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+export default function(){
+    return (
+        <>
+        <div className="bg-gray-900 h-full w-lvw text-white p-3 pt-5">
+            <h1 className="font-extrabold text-4xl text-green-400">LinguaConnect</h1>
+            <div>Connect with the best tutors around the globe</div>
+            
+        </div>
+        </>
+    )
+}
